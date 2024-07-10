50759cc1e14e5796a2f9f903fdd123dee200c810
AlyonaKlekovkina <like.the.sunshine.17@gmail.com>
2024-07-10 15:40:03 +0200

    stage 3

--- X-mas Tree/task/xmasstree.py
+++ X-mas Tree/task/xmasstree.py
@@ -1,12 +1,26 @@
 # This is a sample Python script.
 # import the required library
-height = int(input())
+# This is a sample Python script.
+# import the required library
+height, interval = map(int, input().split())
+count = 1
 n = 1
 space = height - 1
 print(' ' * space + 'X' + ' ' * space)
 print(' ' * space + '^' + ' ' * space)
 for i in range(height - 1):
-    print(' ' * (space - 1) + '/' + '*' * n + '\\' + ' ' * (space -1))
+    line = ' ' * (space - 1) + '/'
+    for j in range(n):
+        if j % 2 == 0:
+            line += '*'
+        elif j % 2 != 0:
+            if interval == 1 or (count % interval == 1):
+                line += 'O'
+                count += 1
+            else:
+                line += '*'
+                count += 1
+    print(line + '\\' + ' ' * (space - 1))
     n += 2
     space -= 1
 print(' ' * (height - 2) + '|' + ' ' + '|' + ' ' * (height - 2))
